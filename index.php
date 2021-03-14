bd199f0cbd3b5c50bc5fc48b0fa9b92ef6a76432
Angie Jimenez <angiejimenez@gmail.com>
2021-03-14 17:06:10 +0100

    include

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php include("header.html");?>
+ <!--Contenido Destacado -->
+ <div class="news-cards">
+
+<div class="card">
+    <h2>Primer Título</h2>
+    <a target="_blank" href="img/Img_primera.jpg">
+    <img src="img/Img_primera.jpg" alt="Primera Imagen" width="200 " height="400">
+</a>
+
+    <a href="#" class="btn"> Acceder <i class="fas fa-chevron-right"></i> </a>
+</div>
+
+<div class="card">
+    <h2>Segundo Título</h2>
+   <a target="_blank" href="img/img_segundo.jpg">
+    <img src="img/img_segundo.jpg" alt="Segunda Imagen" width="200" height="400">
+</a>
+
+    <a href="#" class="btn"> Acceder <i class="fas fa-chevron-right"></i></a>
+
+</div>
+
+<div class="card">
+    <h2>Tercer Título</h2>
+      <a target="_blank" href="img/img_tercero.jpg">
+    <img src="img/img_tercero.jpg" alt="Tercera Imagen" width="200" height="400">
+</a>
+    <a href="#" class="btn"> Acceder <i class="fas fa-chevron-right"></i></a>
+</div>
+</div>
+
+<!-- Contenido Principal -->
+<div class="row">
+<div class="column side">
+    <h2>Side Izquierdo</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+    <a href="#">Ver Más<i class="fas fa-angle-double-right"></i></a>
+</div>
+
+<div class="column middle">
+    <h2>Main Contenido</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus
+        venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget
+        aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque
+        tortor
+        sed accumsan convallis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus
+        venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget
+        aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque
+        tortor
+        sed accumsan convallis.</p>
+    <a href="#">Ver Más<i class="fas fa-angle-double-right"></i></a>
+</div>
+
+<div class="column side">
+    <h2>Side Derecho</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+    <a href="#">Ver Más<i class="fas fa-angle-double-right"></i></a>
+</div>
+</div>
+</div>
+</div>
+
+<?php include("footer.html");?>
+</div>
+</body>
+
+</html>
